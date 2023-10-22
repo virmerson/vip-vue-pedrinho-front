fe73249474470a885eb266fff996145b696c9b6e
Virmerson Bento dos Santos <virmerson.dev@gmail.com>
2023-10-22 20:03:38 +0100

    Listando itens do pedido

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -11,28 +11,36 @@
 				>
 					<v-card color="#1F7087" theme="dark">
 						<div class="d-flex flex-no-wrap justify-space-between">
-							<div>
+							<div v-for="prod in order.products" :key="prod.id">
+
+								<v-avatar class="ma-3" size="125" rounded="0">
+									<v-img
+										:src="prod.img"
+										width="150"
+									></v-img>
+								</v-avatar>
+
 								<v-card-title class="text-h5">
-									{{ order.products[0].desc }}
+									{{ prod.desc }}
 								</v-card-title>
 
 								<v-card-subtitle>
-									{{ order.products[0].price }}
+									{{ prod.price }}
 								</v-card-subtitle>
-								<v-card-actions>
-									<v-icon> mdi-delete </v-icon>
-								</v-card-actions>
+						
+							</div>
+
+							<div>
+
 								<v-card-actions>
 									{{ order.createAt }}
 								</v-card-actions>
 							</div>
 
-							<v-avatar class="ma-3" size="125" rounded="0">
-								<v-img
-									:src="order.products[0].img"
-									width="150"
-								></v-img>
-							</v-avatar>
+
+
+
+							
 						</div>
 					</v-card>
 				</v-col>
@@ -45,21 +53,21 @@
 import { toRefs, computed } from "vue";
 
 export default {
-	props: {
-		orders: Array,
-	},
-	setup(props, { emit }) {
-		const { orders } = toRefs(props);
-		console.log(orders);
+  props: {
+    orders: Array,
+  },
+  setup(props, { emit }) {
+    const { orders } = toRefs(props);
+    console.log(orders);
 
-		const totalCart = computed(() => {
-			const priceSum = orders.value.reduce(
-				(total, order) => total + parseFloat(order.products[0].price),
-				0
-			);
-			return priceSum;
-		});
-		return { orders, totalCart };
-	},
+    const totalCart = computed(() => {
+      const priceSum = orders.value.reduce(
+        (total, order) => total + parseFloat(order.products[0].price),
+        0
+      );
+      return priceSum;
+    });
+    return { orders, totalCart };
+  },
 };
 </script>
